5735b84c49eaf45802b8f21139a50de4546eaf31
toddbontrager <bontragertodd@gmail.com>
2018-04-23 12:04:44 -0400

    Add grid

--- components/MainCards.js
+++ components/MainCards.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Card, Icon } from 'semantic-ui-react'
+import { Card, Icon, Grid } from 'semantic-ui-react'
 
 const consumers = [
   'Amy is a violinist with 2 years experience in the wedding industry.',
@@ -15,27 +15,38 @@ const admin = 'Yo'
 
 const MainCards = () => (
     <div>
-  <Card>
-    <Card.Content header='Consumers' href='consumers' />
-    <Card.Content description={consumers} />
-    <Card.Content extra>
-      <Icon name='user' />
-      4,278 Consumers have checked this week
-    </Card.Content>
-  </Card>
-  
-  <Card>
-    <Card.Content header='Suppliers' href='suppliers'/>
-    <Card.Content description={suppliers} />
-    <Card.Content extra>
-      <Icon name='user' />
-      912 Suppiers on file
-    </Card.Content>
-  </Card>
-  <Card>
-    <Card.Content header='Admin' href='admin'/>
-    <Card.Content description={admin} />
-  </Card>
+      <Grid>
+        
+        <Grid.Column width={5} style={{ marginLeft: '10px' }}>
+          <Card>
+            <Card.Content header='Consumers' href='consumers' />
+            <Card.Content description={consumers} />
+            <Card.Content extra>
+              <Icon name='user' />
+              4,278 Consumers have checked this week
+            </Card.Content>
+          </Card>
+        </Grid.Column>
+
+        <Grid.Column width={5}>
+          <Card>
+            <Card.Content header='Suppliers' href='suppliers'/>
+            <Card.Content description={suppliers} />
+            <Card.Content extra>
+              <Icon name='user' />
+              912 Suppiers on file
+            </Card.Content>
+          </Card>
+        </Grid.Column>
+
+        <Grid.Column width={5}>
+          <Card>
+            <Card.Content header='Admin' href='admin'/>
+            <Card.Content description={admin} />
+          </Card>
+        </Grid.Column>
+
+  </Grid>
   </div>
 )
 
