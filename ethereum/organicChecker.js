9fcd325cf05184578342e433ec611e3d8c8f344a
toddbontrager <bontragertodd@gmail.com>
2018-04-24 14:04:18 -0400

    Update address after contract deployment

--- ethereum/organicChecker.js
+++ ethereum/organicChecker.js
@@ -3,7 +3,7 @@ import OrganicChecker from './build/OrganicChecker.json';
 
 const instance = new web3.eth.Contract(
     JSON.parse(OrganicChecker.interface),
-    '0x5d774869aA07Ce62bBEDc6B9801cA039cCFbe79d'
+    '0x82f5a4B905f098e2feaD7e5f95ace23f33eD63a7'
 );
 
 export async function getSuppliers() {
