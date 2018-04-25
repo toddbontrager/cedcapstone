d96724484dd2203c57c341ecc64e3877f000bb25
toddbontrager <bontragertodd@gmail.com>
2018-04-25 08:52:36 -0400

    Add comments to contract

--- ethereum/contracts/OrganicChecker.sol
+++ ethereum/contracts/OrganicChecker.sol
@@ -15,6 +15,8 @@ contract OrganicChecker {
     mapping (address => Supplier) suppliers;
     address[] public supplierList;
 
+    // Create a supplier. The booleans isCertified and isLabVerified will be set
+    // to false by default when a new supplier is created 
     function setSupplier(address _address, string _name, string _licenseNumber, string _email, string _phone) public {
         suppliers[_address].name = _name;
         suppliers[_address].licenseNumber = _licenseNumber;
@@ -24,18 +26,23 @@ contract OrganicChecker {
         supplierList.push(_address) - 1;
     }
 
+    // Returns an array of all supplier's addresses
     function getAllSuppliers() view public returns(address[]) {
         return supplierList;
     }
 
+    // Returns a supplier's information when its address is given as an argument
     function getSupplierByAddress(address _address) view public returns (string, string, string, string, bool, bool) {
         return (suppliers[_address].name, suppliers[_address].licenseNumber, suppliers[_address].email, suppliers[_address].phone, suppliers[_address].isCertified, suppliers[_address].isLabVerified);
     }
 
+    // Returns the number of suppliers in the supplierList array
     function countSuppliers() view public returns (uint) {
         return supplierList.length;
     }
 
+    // This function is used by the admin. Only the creator of the contract is permitted to invoke this function
+    // Will update the booleans of the supplier whose address is supplied as the argument
     function updateSupplier(address _address, bool _isCertified, bool _isLabVerified) public {
         require(msg.sender == owner);
         suppliers[_address].isCertified = _isCertified;
